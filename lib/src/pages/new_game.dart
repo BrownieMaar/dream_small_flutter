64a66ee8528c3686e6f18ad6ce7cb2a08d0020af
BrownieMaar <barnamarci07@gmail.com>
2023-05-16 10:34:51 +0200

    add form elements and buttons to new game screen

--- lib/src/pages/new_game.dart
+++ lib/src/pages/new_game.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
 
-class NewGame extends StatelessWidget {
+class NewGame extends StatefulWidget {
+  @override
+  State<NewGame> createState() => _NewGameState();
+}
+
+class _NewGameState extends State<NewGame> {
   @override
   Widget build(BuildContext context) {
-    final Map<String, String>? arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+    var theme = Theme.of(context);
+    var nameController = TextEditingController();
+
+    final Map<String, String>? arguments =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
     final int numberAmount = int.parse(arguments?['numberAmount'] ?? "5");
+
+    var numberControllers =
+        List.generate(numberAmount, (index) => TextEditingController());
+
+    var numberInputs = List.generate(numberAmount, (index) {
+      return TextField(
+        controller: numberControllers.elementAt(index),
+        decoration: InputDecoration(
+            border: OutlineInputBorder(), label: Text("Number ${index + 1}.")),
+      );
+    });
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("placeholder"),
+        title: Text("Dream Small"),
+        foregroundColor: theme.colorScheme.onPrimary,
+        backgroundColor: theme.colorScheme.primary,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            Text("placeholder"),
-            Text(numberAmount.toString()),
+            Text(
+              "Add players",
+              style: theme.textTheme.headlineLarge,
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Player name"),
+              ),
+            ),
+            SizedBox(height: 20),
+            ...numberInputs
+                .expand((element) => [element, SizedBox(height: 10)]),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: OutlinedButton.icon(
+                    onPressed: null,
+                    icon: Icon(Icons.shuffle),
+                    label: Text("Fill randomly"),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: FilledButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.send),
+                    label: Text("Submit player"),
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
